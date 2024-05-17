32f7a2c9b812d128aa76499ff7b54b4aaaba0005
Amit Patel <AMITKUMARPATELSAGAR@GMAIL.COM>
2024-05-17 15:20:43 +0530

    project setup

---
+++ resources/views/admin/childcategory/edit.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.load')
+
+@section('content')
+
+            <div class="content-area">
+
+              <div class="add-product-content">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="product-description">
+                      <div class="body-area" id="modalEdit">
+                        @include('includes.admin.form-error')  
+                      <form id="geniusformdata" action="{{route('admin-childcat-update',$data->id)}}" method="POST" enctype="multipart/form-data">
+                        {{csrf_field()}}
+
+                        <div class="row">
+                          <div class="col-lg-4">
+                            <div class="left-area">
+                                <h4 class="heading">{{ __('Category') }}*</h4>
+                            </div>
+                          </div>
+                          <div class="col-lg-7">
+                              <select id="cat" required="">
+                                  <option value="">{{ __('Select Category') }}</option>
+                                    @foreach($cats as $cat)
+                                      <option data-href="{{ route('admin-subcat-load',$cat->id) }}" value="{{ $cat->id }}" {{ $cat->id == $data->subcategory->category->id ? "selected":"" }}>{{ $cat->name }}</option>
+                                    @endforeach
+                                </select>
+                          </div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-lg-4">
+                            <div class="left-area">
+                                <h4 class="heading">{{ __('Sub Category') }} *</h4>
+                            </div>
+                          </div>
+                          <div class="col-lg-7">
+                              <select id="subcat"  name="subcategory_id" required="">
+                                <option value="">{{ __('Select Sub Category') }}</option>
+                                @foreach($data->subcategory->category->subs as $sub)
+                                  <option value="{{$sub->id}}" {{$sub->id == $data->subcategory->id ? "selected":""}}>{{$sub->name}}</option>
+                                @endforeach
+                              </select>
+                          </div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-lg-4">
+                            <div class="left-area">
+                                <h4 class="heading">{{ __('Name') }} *</h4>
+                                <p class="sub-heading">{{ __('(In Any Language)') }}</p>
+                            </div>
+                          </div>
+                          <div class="col-lg-7">
+                            <input type="text" class="input-field" name="name" placeholder="{{ __('Enter Name') }}" required="" value="{{$data->name}}">
+                          </div>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-lg-4">
+                            <div class="left-area">
+                                <h4 class="heading">{{ __('Slug') }} *</h4>
+                                <p class="sub-heading">{{ __('(In English)') }}</p>
+                            </div>
+                          </div>
+                          <div class="col-lg-7">
+                            <input type="text" class="input-field" name="slug" placeholder="{{ __('Enter Slug') }}" required="" value="{{$data->slug}}">
+                          </div>
+                        </div>
+
+                                 
+                      <div class="row">
+                        <div class="col-lg-4">
+                          <div class="left-area">
+                              <h4 class="heading">{{ __('Meta Title') }} *</h4>
+                          </div>
+                        </div>
+                        <div class="col-lg-7">
+                          <input type="text" class="input-field" name="meta_title" placeholder="{{ __('Meta Title') }}" value="{{$data->meta_title}}">
+                        </div>
+                      </div>  
+
+                      <div class="row">
+                        <div class="col-lg-4">
+                          <div class="left-area">
+                            <h4 class="heading">
+                                {{ __('Meta Description') }} *
+                            </h4>
+                          </div>
+                        </div>
+                        <div class="col-lg-7">
+                          <div class="text-editor">
+                            <textarea class="input-field" name="meta_description" placeholder="{{ __('Meta Description') }}">{{$data->meta_description}}</textarea> 
+                          </div>
+                        </div>
+                      </div>
+
+                      
+                        <br>
+                        <div class="row">
+                          <div class="col-lg-4">
+                            <div class="left-area">
+                              
+                            </div>
+                          </div>
+                          <div class="col-lg-7">
+                            <button class="addProductSubmit-btn" type="submit">{{ __('Save') }}</button>
+                          </div>
+                        </div>
+                      </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+@endsection
